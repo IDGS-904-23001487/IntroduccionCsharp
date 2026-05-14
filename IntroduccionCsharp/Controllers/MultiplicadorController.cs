5fbf7d56aaae264e47ad739868e8ec58f7fc0f60
Cristopher Ramirez <cristopherramirezolmos27@gmail.com.com>
2026-05-13 20:34:48 -0600

    Multiplicacion 13/05/2026

---
+++ IntroduccionCsharp/Controllers/MultiplicadorController.cs
@@ -0,0 +1,31 @@
+﻿using IntroduccionCsharp.Controllers;
+using IntroduccionCsharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionCsharp.Controllers
+{
+    public class MultiplicacionController : Controller
+    {
+        public ActionResult Index()
+
+        {
+            return View(new MultiplicacionSuma());
+
+        }
+        [HttpPost]
+
+        public ActionResult Index(MultiplicacionSuma modelo)
+
+        {
+            modelo.Multiplicar();
+            return View(modelo);
+
+        }
+
+    }
+
+}
---
+++ IntroduccionCsharp/Models/MultiplicacionSuma.cs
@@ -0,0 +1,46 @@
+﻿using IntroduccionCsharp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IntroduccionCsharp.Models
+
+{
+
+    public class MultiplicacionSuma
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Resultado { get; set; }
+        public string ResultadoTexto { get; set; }
+
+
+        public void Multiplicar()
+
+        {
+            this.Resultado = 0;
+            this.ResultadoTexto = "";
+            for (int i = 0; i < this.Num2; i++)
+
+            {
+                this.Resultado = this.Resultado + this.Num1;
+                this.ResultadoTexto = this.ResultadoTexto + this.Num1.ToString();
+                if (i < (this.Num2 - 1))
+
+                {
+
+                    this.ResultadoTexto = this.ResultadoTexto + " + ";
+
+                }
+
+            }
+            this.ResultadoTexto = this.ResultadoTexto + " = " + this.Resultado.ToString();
+
+        }
+
+    }
+
+}
